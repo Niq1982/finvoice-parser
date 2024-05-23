46dc169b0c0daf54b759beb938478f44a231b7cf
Niku Hietanen <niku.hietanen@gmail.com>
2024-05-23 19:33:17 +0300

    Create filedata object and collection so we have a consistent way handle files

---
+++ src/Data/FileData.php
@@ -0,0 +1,60 @@
+<?php
+namespace FinvoiceParser\Data;
+
+use FinvoiceParser\Enums\FiletypeEnum;
+use FinvoiceParser\DataObject\DataObject;
+use FinvoiceParser\Exceptions\FileDataException;
+
+/**
+ * A class to represent the files that are being processed.
+ * We save only the file path and name so we won't have to keep a lot of data in memory at once.
+ *
+ * Use the `getData` method to get the actual data from the file, for now only XML files are supported.
+ *
+ * @package FinvoiceParser\Data
+ */
+class FileData extends DataObject
+{
+    public function __construct(
+        public readonly string $filePath,
+        public readonly string $fileName,
+        public readonly FiletypeEnum $fileType,
+    ) {
+        // Strip trailing slashes from the file path
+        $this->filePath = rtrim($this->filePath, '/');
+    }
+
+    public function getFullFilePath(): string
+    {
+        return $this->filePath . '/' . $this->fileName;
+    }
+
+    public function exists()
+    {
+        return file_exists($this->getFullFilePath());
+    }
+
+    public function getData(): object
+    {
+        if (!$this->exists()) {
+            throw new FileDataException("File does not exist: {$this->getFullFilePath()}");
+        }
+
+        return match ($this->fileType) {
+            FiletypeEnum::XML => $this->loadXMLFileData(),
+            default => throw new \InvalidArgumentException("Unsupported file type: {$this->fileType->value}"),
+        };
+    }
+
+    public function loadXMLFileData(): \SimpleXMLElement
+    {
+        // Load and suppress warnings/errors, we will handle them ourselves
+        $xmlElement = simplexml_load_file(filename: $this->getFullFilePath(), options: LIBXML_NOCDATA | LIBXML_NOERROR | LIBXML_NOWARNING);
+
+        if ($xmlElement === false) {
+            throw new FileDataException("Failed to load the XML file");
+        }
+
+        return $xmlElement;
+    }
+}
---
+++ src/Data/FileDataCollection.php
@@ -0,0 +1,29 @@
+<?php
+namespace FinvoiceParser\Data;
+
+use FinvoiceParser\Data\FileData;
+use FinvoiceParser\DataObject\DataObject;
+use FinvoiceParser\DataObject\DataObjectCollection;
+
+class FileDataCollection extends DataObjectCollection
+{
+    public function current(): FileData
+    {
+        return parent::current();
+    }
+
+    /**
+     * @param FileData $item
+     */
+    public function add(DataObject $item): void
+    {
+        if (!$item instanceof FileData) {
+            throw new \InvalidArgumentException('Only FileData objects can be added to this collection');
+        }
+        parent::add($item);
+    }
+    public function first(): FileData|null
+    {
+        return parent::first();
+    }
+}
